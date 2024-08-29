3a01a7d73f55c5cb007abb918105df309954937d
Ana Clara Lemos <136810913+anaclaralemosc@users.noreply.github.com>
2024-08-29 15:35:37 -0300

    Update vetores.c

--- RevisaoED1/vetores.c
+++ RevisaoED1/vetores.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i, temp;
+    int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; //declaração do vetor 
+    int i, temp, n=10; //declaração das variáveis
 
-    for (i = 0; i < 5; i++) {
-        temp = vetor[i];
-        vetor[i] = vetor[9 - i];
-        vetor[9 - i] = temp;
+    for (i = 0; i < (n/2); i++) { //laço de repetição, n/2 para conseguir inverter as duas metades
+        temp = vetor[i]; //temp é uma variavel auxiliar 
+        vetor[i] = vetor[n-1 - i]; //invertendo o vetor 
+        vetor[n-1 - i] = temp; 
     }
 
-    printf("Vetor invertido:\n");
+    printf("Vetor invertido:\n"); //impressão do vetor invertido 
     for (i = 0; i < 10; i++) {
         printf("%d ", vetor[i]);
     }
